74672c28b21c9477771b7715e88b9f6396eba390
Anna Kapitanova <totoshechka19@gmail.com>
2024-05-08 10:57:09 +0300

    Добавила классам приватные поля и геттеры

--- src/presenter/main-presenter.js
+++ src/presenter/main-presenter.js
@@ -5,33 +5,36 @@ import EventView from '../view/event-view.js';
 import FormView from '../view/form-view.js';
 
 export default class MainPresenter {
-  sortComponent = new SortView();
-  eventsListComponent = new EventsListView();
+  #container = null;
+  #eventModel = null;
+
+  #sortComponent = new SortView();
+  #eventsListComponent = new EventsListView();
+
+  #events = [];
 
   constructor({container, eventModel}) {
-    this.container = container;
-    this.eventModel = eventModel;
-    this.destinations = this.eventModel.getDestinations();
-    this.offers = this.eventModel.getOffers();
+    this.#container = container;
+    this.#eventModel = eventModel;
   }
 
   // Точка входа для инициализации представления
   init() {
-    this.events = [...this.eventModel.getEvents()];
+    this.#events = [...this.#eventModel.events];
 
     // Рендеринг сортировки
-    render(this.sortComponent, this.container);
+    render(this.#sortComponent, this.#container);
 
     // Рендеринг списка путешествий
-    render(this.eventsListComponent, this.sortComponent.element, RenderPosition.AFTEREND);
+    render(this.#eventsListComponent, this.#sortComponent.element, RenderPosition.AFTEREND);
 
     // Рендеринг формы создания/редактирования точки путешествия
     // Рендеринг точек путешествия
-    this.events.forEach((event, index) => {
+    this.#events.forEach((event, index) => {
       const view = index === 0
-        ? new FormView({event, destinations: this.destinations, offers: this.offers})
-        : new EventView({event, destinations: this.destinations, offers: this.offers});
-      render(view, this.eventsListComponent.element, RenderPosition.BEFOREEND);
+        ? new FormView({event, destinations: this.#eventModel.destinations, offers: this.#eventModel.offers})
+        : new EventView({event, destinations: this.#eventModel.destinations, offers: this.#eventModel.offers});
+      render(view, this.#eventsListComponent.element, RenderPosition.BEFOREEND);
     });
   }
 }
