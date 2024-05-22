0295638d0f49c701ae244f1e4641f934733127d5
Anna Kapitanova <totoshechka19@gmail.com>
2024-05-22 15:15:15 +0300

    удалила комментарии из кода

--- src/main.js
+++ src/main.js
@@ -6,28 +6,21 @@ import EventModel from './model/event-model.js';
 import FilterModel from './model/filter-model.js';
 import { Filters } from './const.js';
 
-// Поиск элементов в DOM, которые будут использоваться для рендеринга компонентов шапки сайта
 const tripMainElement = document.querySelector('.trip-main');
 const tripFiltersElement = tripMainElement.querySelector('.trip-controls__filters');
 const tripEventsElement = document.querySelector('.trip-events');
 
-// Рендеринг шапки сайта
 render(new TripInfoView(), tripMainElement, RenderPosition.AFTERBEGIN);
 
-// Создание экземпляра EventModel
 const eventModel = new EventModel();
-
-// Создание экземпляра FilterModel
 const filterModel = new FilterModel();
 
-// Создание экземпляра MainPresenter
 const mainPresenter = new MainPresenter({
   container: tripEventsElement,
   eventModel,
   filterModel
 });
 
-// Создание экземпляра FilterView с обработчиком изменения фильтра
 const filterView = new FilterView({
   filters: Object.values(Filters),
   currentFilter: filterModel.filter,
@@ -37,7 +30,6 @@ const filterView = new FilterView({
   isDisabled: !eventModel.hasEvents()
 });
 
-// Рендеринг фильтров
 render(filterView, tripFiltersElement);
 
 mainPresenter.init();
--- src/utils.js
+++ src/utils.js
@@ -5,24 +5,18 @@ dayjs.extend(duration);
 
 const getRandomArrayElement = ((items) => items[Math.floor(Math.random() * items.length)]);
 
-// Функция для форматирования даты
 const getFormattedDate = (dateString, format) => dayjs(dateString).format(format);
 
-// Функция для вычисления продолжительности в днях, часах и минутах
 const getDuration = (dateFrom, dateTo) => {
-  // Обрезаем до минут начальную и конечную даты
   const start = dayjs(dateFrom).startOf('minute');
   const end = dayjs(dateTo).startOf('minute');
-  const diffInMs = end.diff(start); // Вычисляем разницу в миллисекундах
+  const diffInMs = end.diff(start);
   // eslint-disable-next-line no-shadow
-  const duration = dayjs.duration(diffInMs); // Создаем объект длительности с разницей в миллисекундах
-
-  // Получаем количество целых дней, часов, минут
+  const duration = dayjs.duration(diffInMs);
   const days = Math.floor(duration.asDays());
   const hours = duration.hours();
   const minutes = duration.minutes();
 
-  // Форматируем длительность в строку в формате "DDDH HHM" или "HHM" или "MM"
   if (days > 0) {
     return `${days.toString().padStart(2, '0')}D ${hours.toString().padStart(2, '0')}H ${minutes.toString().padStart(2, '0')}M`;
   }
@@ -34,11 +28,12 @@ const getDuration = (dateFrom, dateTo) => {
   return `${minutes}M`;
 };
 
-// Функция получения активного класса
 const getActiveClass = (isActive, activeClass) => isActive ? activeClass : '';
 
 const isEventFuture = (dateFrom) => new Date(dateFrom) > new Date();
+
 const isEventPresent = (dateFrom, dateTo) => new Date(dateFrom) <= new Date() && new Date(dateTo) >= new Date();
+
 const isEventPast = (dateTo) => new Date(dateTo) < new Date();
 
 const filter = {
@@ -48,7 +43,6 @@ const filter = {
   [Filters.PAST]: (events) => events.filter((event) => isEventPast(event.dateTo))
 };
 
-// Функции для обновления точек маршрута
 const updateData = (data, update) => data.map((item) => item.id === update.id ? update : item);
 const updateItem = (item, prop) => ({...item, ...prop});
 
